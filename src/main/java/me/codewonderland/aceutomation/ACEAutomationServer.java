b68133e94ecd37e492d631575c2e0b8ce1e842dc
Sam Clark <sam.clark1142@gmail.com>
2017-01-28 15:38:31 -0500

    power get

--- src/main/java/me/codewonderland/aceutomation/ACEAutomationServer.java
+++ src/main/java/me/codewonderland/aceutomation/ACEAutomationServer.java
@@ -2,14 +2,29 @@ package me.codewonderland.aceutomation;
 
 import org.json.JSONObject;
 
+import java.io.StringWriter;
+
+import static spark.Spark.get;
 import static spark.Spark.post;
 
 public class ACEAutomationServer {
+
+    private static boolean diffuserIsOn = false;
 
     public static void main(String[] args) {
         post("/power", (req, res) -> {
             JSONObject request = new JSONObject(req.body());
             return "success";
+        });
+
+        get("/power", (req, res) -> {
+            JSONObject response = new JSONObject();
+            response.put("diffuserIsOn", diffuserIsOn);
+
+            StringWriter out = new StringWriter();
+            response.write(out);
+
+            return out.toString();
         });
     }
 
