6f0dc03531de3dc40df6cf023910cd0467516932
Alice Easter <alice.easter@mymail.champlain.edu>
2017-02-07 22:04:35 -0500

    expanded upon server functionality, added get requests for every diffuser command and for tv power

--- src/main/java/me/codewonderland/aceautomation/ACEAutomationServer.java
+++ src/main/java/me/codewonderland/aceautomation/ACEAutomationServer.java
@@ -5,30 +5,95 @@ import org.json.JSONObject;
 import java.io.StringWriter;
 
 import static spark.Spark.get;
-import static spark.Spark.post;
 
 public class ACEAutomationServer {
 
-    private static boolean diffuserIsOn = false;
-
     public static void main(String[] args) {
 
-        LIRCManager.sendCommand(AutomationCommand.DIFFUSER_PLAY);
-
-        post("/togglePower", (req, res) -> {
-            JSONObject request = new JSONObject(req.body());
-            return "success";
-        });
-
-        get("/power", (req, res) -> {
+        get("/diffuserPower", (req, res) -> {
             JSONObject response = new JSONObject();
-            response.put("diffuserIsOn", diffuserIsOn);
+            response.put("diffuserPowerToggled", true);
+
+            LIRCManager.sendCommand(AutomationCommand.DIFFUSER_POWER);
 
             StringWriter out = new StringWriter();
             response.write(out);
 
             return out.toString();
         });
+
+        get("/diffuserPlay", (req, res) -> {
+            JSONObject response = new JSONObject();
+            response.put("diffuserMusicToggled", true);
+
+            LIRCManager.sendCommand(AutomationCommand.DIFFUSER_PLAY);
+
+            StringWriter out = new StringWriter();
+            response.write(out);
+
+            return out.toString();
+        });
+
+        get("/diffuserLights", (req, res) -> {
+            JSONObject response = new JSONObject();
+            response.put("diffuserLightsToggled", true);
+
+            LIRCManager.sendCommand(AutomationCommand.DIFFUSER_LIGHTS);
+
+            StringWriter out = new StringWriter();
+            response.write(out);
+
+            return out.toString();
+        });
+
+        get("/diffuserLightCycle", (req, res) -> {
+            JSONObject response = new JSONObject();
+            response.put("diffuserLightCycleToggled", true);
+
+            LIRCManager.sendCommand(AutomationCommand.DIFFUSER_LIGHT_CYCLE);
+
+            StringWriter out = new StringWriter();
+            response.write(out);
+
+            return out.toString();
+        });
+
+        get("/diffuserVolumeUp", (req, res) -> {
+            JSONObject response = new JSONObject();
+            response.put("diffuserVolumeUpToggled", true);
+
+            LIRCManager.sendCommand(AutomationCommand.DIFFUSER_VOLUMEUP);
+
+            StringWriter out = new StringWriter();
+            response.write(out);
+
+            return out.toString();
+        });
+
+        get("/diffuserVolumeDown", (req, res) -> {
+            JSONObject response = new JSONObject();
+            response.put("diffuserVolumeDownToggled", true);
+
+            LIRCManager.sendCommand(AutomationCommand.DIFFUSER_VOLUMEDOWN);
+
+            StringWriter out = new StringWriter();
+            response.write(out);
+
+            return out.toString();
+        });
+
+        get("/televisionPower", (req, res) -> {
+            JSONObject response = new JSONObject();
+            response.put("televisionPowerToggled", true);
+
+            LIRCManager.sendCommand(AutomationCommand.TELEVISION_POWER);
+
+            StringWriter out = new StringWriter();
+            response.write(out);
+
+            return out.toString();
+        });
+
     }
 
 } 
